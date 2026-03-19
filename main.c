b3ee118e607c9bd89c78c58ab2b7868441692b63
Chabo10-el <boukariflorent2006@gmail.com>
2026-03-19 07:29:19 +0000

    Ajout exercice 1 en C

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int verification(int tab[],int nb,int x){
+    for(int i=1;i<=nb;i++){
+        if(tab[i]==x){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
+int main()
+{
+    int tab[]={9,5,12,8,4,19},v;
+    const int nb=sizeof(tab)/sizeof(tab[0]);
+    v=verification(tab,nb,19);
+    printf("%d",v);
+    return 0;
+}
